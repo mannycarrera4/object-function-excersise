9afecff91ae8b7cc20e52fc1dd31013ad3f58c96
mannycarrera4 <manuel.carrera.91@gmail.com>
2014-10-13 18:17:12 -0500

    learning about functions and objects

--- myscript.js
+++ myscript.js
@@ -1,1 +1,57 @@
-console.log("Hello World");
+// var car = {
+//     model: "911",
+//     make: "porsche",
+//     color: "black",
+//     year: "2014"
+// };
+
+var carFactory = {
+    build: function(car) {
+        // console.log('carFactory build method', car);
+    },
+
+    updateProperties: function(car) {
+        car.speed = 100;
+        car.color = 'blue';
+
+        return car.color;
+    },
+
+    getCar: function() {
+        var car = {
+            model: "911",
+            make: "porsche",
+            color: "black",
+            year: "2014"
+        };
+
+        return car;
+    },
+
+    init: function() {
+        var car = carFactory.getCar();
+        carFactory.updateProperties(car);
+        carFactory.build(car);
+
+        return [car, carFactory.updateProperties(car), carFactory.build(car)];
+    },
+
+    yearMade: '1987',
+
+    manufacturer: 'Porsche' 
+};
+
+carFactory.init();
+console.log('calling out main object', carFactory);
+
+function hereIs(carFactory) {
+    carFactory.engineSize = '4 cylinder';
+}
+
+hereIs(carFactory);
+
+console.log('using function to call an object', carFactory.engineSize);
+
+
+
+
